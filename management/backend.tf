046cd1bce6f0eea03bcab710dcfce54ed865ff2a
RFN <ryder@rgn.one>
2026-03-07 16:41:57 +0100

    remove mgmt machine

--- management/backend.tf
+++
@@ -1,7 +0,0 @@
-# Local backend — deploy management from your machine first.
-# After the instance exists, you can run the main Hercules config from it.
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
-}
